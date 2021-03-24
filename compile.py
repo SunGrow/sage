1b8dc07d7a70e5849f4f5dc81e1ac1594be19429
SunGrow <sungrow@tuta.io>
2021-03-24 13:53:58 +0200

    Moved data to a more logical place
    Now resources belong to the app

--- compile.py
+++ compile.py
@@ -10,12 +10,13 @@ def copyAndOverride(from_path, to_path):
 
 os.system("meson build")
 
-os.chdir("shaders")
+os.chdir("app/shaders")
 os.system("make")
-os.chdir("..")
-copyAndOverride("shaders", "build/app/shaders")
-copyAndOverride("res", "build/app/res")
-copyAndOverride("cfg", "build/app/cfg")
+os.chdir("../../")
+copyAndOverride("app/shaders", "build/app/shaders")
+copyAndOverride("app/res", "build/app/res")
+copyAndOverride("app/data", "build/app/data")
+copyAndOverride("app/cfg", "build/app/cfg")
 
 os.chdir("build")
 os.system("meson compile")
